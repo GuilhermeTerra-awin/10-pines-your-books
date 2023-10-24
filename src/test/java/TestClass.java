95f41281f6e302e177a7b57eb5f9c47b3dc918b3
Guilherme Terra <guilherme.terra@awin.com>
2023-10-24 16:10:15 +0200

    adding ids to carts

--- src/test/java/TestClass.java
+++ src/test/java/TestClass.java
@@ -2,7 +2,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
+import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -10,26 +10,41 @@ public class TestClass {
 
     @Test
     void test1() {
-        var cart = new Cart();
+        var cart = new Cart("1");
         assertTrue(cart.isEmpty());
     }
 
     @Test
     void test2() {
-        var cart = new Cart();
+        var cart = new Cart("1");
         cart.add("apple");
         assertFalse(cart.isEmpty());
         assertTrue(cart.contains("apple"));
     }
 
     @Test
-    void test3(){
+    void test3() {
         var cartManager = new CartManager();
-        assertEquals("id",cartManager.createCart().getId());
+        assertFalse(cartManager.createCart().getId().isEmpty());
+    }
+
+    @Test
+    void test4() {
+        var cartManager = new CartManager();
+
+        String id1 = cartManager.createCart().getId();
+        String id2 = cartManager.createCart().getId();
+
+        assertNotEquals(id1, id2);
     }
 
     private class Cart {
         private final List<String> items = new ArrayList<>();
+        private final String id;
+
+        public Cart(String id) {
+            this.id = id;
+        }
 
         public boolean isEmpty() {
             return items.isEmpty();
@@ -44,15 +59,14 @@ public class TestClass {
         }
 
         public String getId() {
-            return "id";
+            return id;
         }
     }
 
     private class CartManager {
 
-
         public Cart createCart() {
-            return new Cart();
+            return new Cart(UUID.randomUUID().toString());
         }
     }
 }
